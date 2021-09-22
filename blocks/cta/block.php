b5554fb3fedf0a6b8f36f2f5a556829120df4da0
Estevão Rolim <estevao.prolim@gmail.com>
2021-09-22 23:34:32 +0100

    Modify CTA

--- blocks/cta/block.php
+++ blocks/cta/block.php
@@ -12,34 +12,34 @@ function render_block_cta($attributes, $content)
     $block_id = 'gdiCTA' . rand(0, 100);
 
     ob_start(); // Start HTML buffering
-?>
+    ?>
 
-    <section class="gdi-cta carousel slide" 
-        data-bs-ride="<?php echo ($auto_slide) ? "carousel" : "false" ?>" 
-        data-bs-interval="<?php echo ($auto_slide) ? $interval : "" ?>" 
-        id="<?php echo $block_id; ?>">
+        <section class="gdi-cta carousel slide" 
+            data-bs-ride="<?php echo ($auto_slide) ? "carousel" : "false" ?>" 
+            data-bs-interval="<?php echo ($auto_slide) ? $interval : "" ?>" 
+            id="<?php echo $block_id; ?>">
 
-        <div class="carousel-inner">
-            <?php echo $content ?>
-        </div>
-                    
-        <div class="position-absolute bottom-0 w-100 d-flex">
-            <div class="mx-auto d-flex mb-2">
-                <button class="carousel-control-prev me-1" type="button"
-                data-bs-target="#<?php echo $block_id; ?>" data-bs-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden"><?php echo __("Anterior"); ?></span>
-                </button>
-                <button class="carousel-control-next ms-1" type="button"
-                data-bs-target="#<?php echo $block_id; ?>" data-bs-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="visually-hidden"><?php echo __("Próximo"); ?></span>
-                </button>
+            <div class="carousel-inner">
+                <?php echo $content ?>
             </div>
-        </div>
-    </section>
+                        
+            <div class="position-absolute bottom-0 w-100 d-flex">
+                <div class="mx-auto d-flex mb-2">
+                    <button class="carousel-control-prev me-1" type="button"
+                    data-bs-target="#<?php echo $block_id; ?>" data-bs-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden"><?php echo __("Anterior"); ?></span>
+                    </button>
+                    <button class="carousel-control-next ms-1" type="button"
+                    data-bs-target="#<?php echo $block_id; ?>" data-bs-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="visually-hidden"><?php echo __("Próximo"); ?></span>
+                    </button>
+                </div>
+            </div>
+        </section>
 
-<?php
+    <?php
 
     $output = ob_get_contents(); // collect buffered contents
 
